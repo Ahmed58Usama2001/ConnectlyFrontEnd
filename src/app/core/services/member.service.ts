d99d53bb25de1bd234356c7717edfde1c87a584b
Ahmed Usama <ausama@innotech.com.eg>
2025-08-28 16:23:38 +0300

    member Service

--- src/app/core/services/member.service.ts
+++ src/app/core/services/member.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment.development';
+import { Member } from '../../shared/models/membet';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,12 @@ import { environment } from '../../../environments/environment.development';
 export class MemberService {
   private baseUrl = environment.apiUrl + 'members/';
   private http = inject(HttpClient);
-  private router = inject(Router);
-  
-  constructor() { }
+
+  getMembers(){
+    return this.http.get<Member[]>(this.baseUrl);
+  }
+
+  getMember(id: string){
+    return this.http.get<Member>(this.baseUrl+id);
+  }
 }

a253427907f9831f26cfb6f4869ed2fc2e5bd066
Ahmed Usama <ausama@innotech.com.eg>
2025-09-22 18:44:09 +0300

    liked members list

--- src/app/features/lists/lists.component.ts
+++ src/app/features/lists/lists.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { LikesService } from '../../core/services/likes.service';
+import { Member } from '../../shared/models/membet';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit {
+  private likesService = inject(LikesService);
+  protected members = signal<Member[]>([]);
+  protected predicate = 'liked';
 
+  tabs = [
+    { label: 'Liked', value: 'liked' },
+    { label: 'Liked By', value: 'likedby' },
+    { label: 'Mutual Likes', value: 'mutual'}
+  ];
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  setPredicate(predicate: string) {
+    this.predicate = predicate;
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => {
+        this.members.set(members);
+      }
+    });
+  }
 }

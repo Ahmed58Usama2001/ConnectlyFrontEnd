6a58907f97a9ab5afca411dc66b5eb214628d658
Ahmed Usama <ausama@innotech.com.eg>
2025-10-11 21:44:44 +0300

    refresh token

--- src/app/core/services/accoun.service.ts
+++ src/app/core/services/accoun.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { Router } from '@angular/router';
-import { tap } from 'rxjs';
+import { catchError, interval, of, Subscription, tap } from 'rxjs';
 import { User, RegisterDto, LoginDto } from '../../shared/models/user';
 import { LikesService } from './likes.service';
 
@@ -16,25 +16,30 @@ export class AccounService {
   private likesService = inject(LikesService);
 
   private userSignal = signal<User | null>(this.getUserFromLocalStorage());
-
   readonly currentUser = computed(() => this.userSignal());
 
+  private refreshIntervalSubscription: Subscription | null = null;
+
+  constructor() {
+    this.tryRefreshTokenOnStartup();
+  }
+
   register(registerDto: RegisterDto) {
-    return this.http.post<User>(this.baseUrl + 'register', registerDto).pipe(
+    return this.http.post<User>(this.baseUrl + 'register', registerDto, { withCredentials: true }).pipe(
       tap(user => this.setUser(user))
     );
   }
 
   login(loginDto: LoginDto) {
-    return this.http.post<User>(this.baseUrl + 'login', loginDto).pipe(
+    return this.http.post<User>(this.baseUrl + 'login', loginDto, { withCredentials: true }).pipe(
       tap(user => this.setUser(user))
     );
   }
 
   logout() {
-    return this.http.post(this.baseUrl + 'logout', {}).pipe(
+    return this.http.post(this.baseUrl + 'logout', {}, { withCredentials: true }).pipe(
       tap(() => {
-        localStorage.removeItem('user');
+        this.stopAutoRefresh();
         this.likesService.clearLikeIds();
         this.setUser(null);
         this.router.navigate(['/home']);
@@ -43,9 +48,48 @@ export class AccounService {
   }
 
   getCurrentUser() {
-    return this.http.get<User>(this.baseUrl + 'get-current-user').pipe(
+    return this.http.get<User>(this.baseUrl + 'get-current-user', { withCredentials: true }).pipe(
       tap(user => this.setUser(user))
     );
+  }
+
+  refreshToken() {
+    return this.http.post<User>(this.baseUrl + 'refresh-token', {}, { withCredentials: true }).pipe(
+      tap(user => this.setUser(user)),
+      catchError(() => {
+        this.stopAutoRefresh();
+        this.setUser(null);
+        return of(null);
+      })
+    );
+  }
+
+  private tryRefreshTokenOnStartup() {
+    const user = this.getUserFromLocalStorage();
+    if (user) {
+      this.startAutoRefresh();
+    } else {
+      this.refreshToken().subscribe(res => {
+        if (res) this.startAutoRefresh();
+      });
+    }
+  }
+
+
+  private startAutoRefresh() {
+    this.stopAutoRefresh(); 
+    const SIX_MINUTES =  14 * 60 * 1000;
+
+    this.refreshIntervalSubscription = interval(SIX_MINUTES).subscribe(() => {
+      this.refreshToken().subscribe();
+    });
+  }
+
+  private stopAutoRefresh() {
+    if (this.refreshIntervalSubscription) {
+      this.refreshIntervalSubscription.unsubscribe();
+      this.refreshIntervalSubscription = null;
+    }
   }
 
   private getUserFromLocalStorage(): User | null {
@@ -54,17 +98,21 @@ export class AccounService {
   }
 
   setUser(user: User | null) {
-    user!.roles = user ? this.getRolesFromToken(user) : [];
     if (user) {
+      user.roles = this.getRolesFromToken(user);
       localStorage.setItem('user', JSON.stringify(user));
+      this.userSignal.set(user);
+      this.likesService.getLikeIds();
+      this.startAutoRefresh();
     } else {
       localStorage.removeItem('user');
+      this.userSignal.set(null);
+      this.stopAutoRefresh();
     }
-    this.userSignal.set(user);
-    this.likesService.getLikeIds();
   }
 
   private getRolesFromToken(user: User): string[] {
+    if (!user?.token) return [];
     const payload = user.token.split('.')[1];
     const decodedPayload = atob(payload);
     const tokenData = JSON.parse(decodedPayload);
